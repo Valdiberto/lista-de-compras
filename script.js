2dad59f1b0ca4b6b0f796606f19516c9ca1637be
Vald <junior-dk555@hotmail.com>
2024-12-20 12:54:20 -0300

    code to add products

--- script.js
+++ script.js
@@ -4,9 +4,36 @@ const buttonSubmit = document.querySelector(".submit")
 // pega o valor do que foi digitado na caixa de texto
 produtosAdd.addEventListener("keydown", (e) => {})
 
+function updateList (produtos){
+  let listArea = document.querySelector(".list-area")
+  let itensArea = document.querySelector(".itens")
+  let showItensArea = document.querySelector(".showItens")
+
+
+  if (produtos > "" && noItem === "noItem" ) {
+    console.log("passou")
+    noItem = ""
+
+      let postList =  `<div class="showItens"> <input type="checkbox" class="itens-checks" name="compras" />
+      <label for="compras">${produtos}</label> </div>`
+      listArea.innerHTML = noItem
+      listArea.innerHTML += postList
+    
+    
+  } else if (produtos > "" && noItem == "" ) {
+    let postList =  `<div class="showItens"> <input type="checkbox" class="itens-checks" name="compras" />
+    <label for="compras">${produtos}</label> </div>`
+    listArea.innerHTML += postList
+
+  }else {
+      noItem = "noItem"
+      listArea.innerHTML = noItem                  
+  }
+}
+
+
 buttonSubmit.addEventListener("click", (e) => {
   // verifica se algo foi digitado
-  const hasCharactersReges = /\D+/g
 
   var produtos = produtosAdd.value
 
@@ -14,6 +41,10 @@ buttonSubmit.addEventListener("click", (e) => {
   if (produtos.length === 0 || !produtos.trim()) {
     alert("adicione algo")
   } else {
+    updateList(produtos)
     console.log(`Adicionou: ${produtos}`)
   }
 })
+
+
+updateList()
